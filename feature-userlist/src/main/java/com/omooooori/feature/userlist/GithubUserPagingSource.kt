8fc2e6ff63d26778b5ceaa1e88a67144d233d852
omooooori <omooooori432@gmail.com>
2025-05-11 14:55:54 +0900

    query filter bug fix.

--- feature-userlist/src/main/java/com/omooooori/feature/userlist/GithubUserPagingSource.kt
+++ feature-userlist/src/main/java/com/omooooori/feature/userlist/GithubUserPagingSource.kt
@@ -8,6 +8,7 @@ import com.omooooori.model.GithubUser
 
 class GithubUserPagingSource(
     private val fetchUsersUseCase: FetchUsersUseCase,
+    private val query: String,
 ) : PagingSource<Int, GithubUser>() {
     override fun getRefreshKey(state: PagingState<Int, GithubUser>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
@@ -17,18 +18,36 @@ class GithubUserPagingSource(
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, GithubUser> {
+        val page = params.key ?: 1
         return try {
-            val page = params.key ?: 1
             val responseDto = fetchUsersUseCase.execute(page)
-            val response = responseDto.map { it.toModel() }
-
+            val allUsers = responseDto.map { it.toModel() }
+            val filteredUsers =
+                if (query.isNotBlank()) {
+                    allUsers.filter {
+                        it.username.contains(query, ignoreCase = true)
+                    }
+                } else {
+                    allUsers
+                }
+            val nextKey =
+                when {
+                    responseDto.size < PAGE_SIZE -> null
+                    query.isNotEmpty() -> null
+                    else -> page + 1
+                }
+            val prevKey = if (page == 1) null else page - 1
             LoadResult.Page(
-                data = response,
-                prevKey = if (page == 1) null else page - 1,
-                nextKey = if (response.isEmpty()) null else page + 1,
+                data = filteredUsers,
+                prevKey = prevKey,
+                nextKey = nextKey,
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
         }
+    }
+
+    companion object {
+        const val PAGE_SIZE = 30
     }
 }
